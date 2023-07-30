8811e8abf42744facb62d7caccf7fe099424439b
Farhan Aldy <farhanpoetra011@gmail.com>
2023-07-31 01:17:16 +0700

    create search bar

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -8,7 +8,54 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: [
           HomeAppBar(),
-          
+          Container(
+            // temporary height
+            height: 500,
+            padding: EdgeInsets.only(top: 15),
+            decoration: BoxDecoration(
+              color: Color(0xFFEDECF2),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(35),
+                topRight: Radius.circular(35),
+              ),
+            ),
+            child: Column(
+              children: [
+
+                // Search Widget
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 15),
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(left: 5),
+                        height: 50,
+                        width: 300,
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Cari disini....",
+                          ),
+                        ),
+                      ),
+                      Spacer(),
+                      Icon(
+                        Icons.camera_alt,
+                        size: 25,
+                        color: Color(0xFF4C53A5),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
